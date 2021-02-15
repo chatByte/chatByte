80d78a376c40b0eb952562b768cd8aa0f6df52dd
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-02-15 12:51:27 -0700

    tested connecting mongoDB in python

--- mysite/chat/views.py
+++ mysite/chat/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Author
 # Create your views here.
 def index(request):
-    return render(request, 'chat/index.html')
+    latest_list = Author.objects.order_by('host')[:5]
+    context = {'latest_list': latest_list}
+    return render(request, 'chat/index.html', context)
