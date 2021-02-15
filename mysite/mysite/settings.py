fc66a07fce13648d7be50438ac921ef1e8d847a8
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-02-15 12:17:59 -0700

    deleted extra database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -71,16 +71,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -123,6 +113,6 @@ STATIC_URL = '/static/'
 DATABASES = {
        'default': {
            'ENGINE': 'djongo',
-           'NAME': 'db',
+           'NAME': 'mongoDB',
        }
    }
