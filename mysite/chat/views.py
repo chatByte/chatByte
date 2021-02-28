031f6e9234be810542b6b01bc49821c66d6cf751
claire45 <yliu17@ualberta.ca>
2021-02-27 19:58:27 -0700

    testing the post function

--- mysite/chat/views.py
+++ mysite/chat/views.py
@@ -2,22 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Author
 from .models import Post
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    latest_list = Post.objects.all()
+    latest_list = Author.objects.all()
     context = {'latest_list': latest_list}
-    # Create an author
-    # Author.objects.create(HOST='cat', DISPLAY_NAME='cat', URL='cat', GITHUB='cat')
-
-    # Create a post
-    # author = Author.objects.get(HOST="cat")
-    # Post.objects.create(TITLE='', SOURCE='testing', ORIGIN='', DESCIPTION='', CONTENT_TYPE='', CONTENT=''\
-    #     , AUTHOR=author, CATEGORIES='', COMMENTS_NO=0, PAGE_SIZE=0, COMMENTS_FIRST_PAGE='', VISIBILITY='public')
-
-    # change a field in the post
-    # post = Post.objects.filter(SOURCE='testing')[0]
-    # print(post)
-    # post.SOURCE = "changed"
-    # post.save()
+    Author.objects.create(HOST='cat', DISPLAY_NAME='cat', URL='cat', GITHUB='cat')
     return render(request, 'chat/index.html', context)
+
+# ...
+def usr_post(request, ID):
+    post = get_object_or_404(Post, pk=ID)
+    try:
+        post_content = Post.get(pk=request.POST['post'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'post': post,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        post_content.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('results', args=(post.id,))) # 'polls:results'
