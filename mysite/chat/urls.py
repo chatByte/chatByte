130b307ec191128b8b0d55109a499ca608e74e80
Siyuan Liu <lsy997168748@gmail.com>
2021-03-01 09:52:43 -0800

    apply static

--- mysite/chat/urls.py
+++ mysite/chat/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
   path("mystream/", views.my_timeline, name="my_timeline"),
   path("seeothers/", views.others_timeline, name="others_timeline"),
   path("feed/", views.make_post, name="feed"),
-  path("profile", views.profile, name="profile"),
+  path("profile/", views.profile, name="profile"),
   #path('', views.index, name='index'),
   #path('home', views.home_view, name='home')
 ]
