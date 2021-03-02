ca14be86ade1e5eb7d1a684bf34b2faa3726d7fa
youwei1-sudo <youwei1@ualberta.ca>
2021-03-02 15:40:51 -0800

    successful post to data base

--- mysite/chat/views.py
+++ mysite/chat/views.py
@@ -12,29 +12,30 @@ from django.contrib import messages
 
 
 """
-views.py receive request and create repose to client
+views.py receive request and create repose to client, 
+Create your views here.
 """
-cur_author = None
 
-# deisign
+# default value
+cur_user_name = "Ritsu Onodera" 
 
-# cur_author obj created
-# Create your views here.
+
 def home(request):
 
     context = {}
     context['form']= InputForm()
     if request.method == "GET":
-    	return render(request, "chat/home.html", context)
+        return render(request, "chat/home.html", context)
     elif request.method == "POST":
-    	username = request.POST.get("User_name")
-    	password = request.POST.get("Password")
-    	valid = validUser(username, password)
-    	if valid:
-    		return redirect("/chat/profile")
-    	else:
-    		messages.error(request, "Invalid user name or password!")
-    		return render(request, 'chat/home.html', context)
+        username = request.POST.get("User_name")
+        password = request.POST.get("Password")
+        valid = validUser(username, password)
+        if valid:
+            cur_user_name = username
+            return redirect("/chat/profile")
+        else:
+            messages.error(request, "Invalid user name or password!")
+            return render(request, 'chat/home.html', context)
    
 # # Create your views here.
 # def home_view(request):
@@ -43,32 +44,32 @@ def home(request):
 #     return render(request, "chat/signup.html", context)
 
 def signup(request):
-	context = {}
-	context['form'] = CreateAuthorForm()
-	if request.method == "GET":
-		return render(request, "chat/signup.html", context)
-	elif request.method == "POST":
-		url = request.POST.get("Url")
-		username = request.POST.get("User_name")
-		github = request.POST.get("Github")
-		password = request.POST.get("Password")
-		if getAuthor(username) != None:
-			messages.error(request, 'User name exists!')
-			return render(request, "chat/signup.html", context)
-		else:
-			print(createUser(username, password))
-			createAuthor("this", username, url, github)
-			return redirect("/chat/profile/")
-		# if createAuthor("this", username, url, github):
-		# 	return redirect("/chat/profile/")
-		# else:
-		# 	messages.error(request, 'User name exists!')
-		# 	return render(request, "chat/signup.html", context)
+    context = {}
+    context['form'] = CreateAuthorForm()
+    if request.method == "GET":
+        return render(request, "chat/signup.html", context)
+    elif request.method == "POST":
+        url = request.POST.get("Url")
+        username = request.POST.get("User_name")
+        github = request.POST.get("Github")
+        password = request.POST.get("Password")
+        if getAuthor(username) != None:
+            messages.error(request, 'User name exists!')
+            return render(request, "chat/signup.html", context)
+        else:
+            print(createUser(username, password))
+            createAuthor("this", username, url, github)
+            return redirect("/chat/profile/")
+        # if createAuthor("this", username, url, github):
+        #   return redirect("/chat/profile/")
+        # else:
+        #   messages.error(request, 'User name exists!')
+        #   return render(request, "chat/signup.html", context)
 
 def my_timeline(request):
 
     # hard coding ??
-    cur_author = getAuthor('Ritsu Onodera')
+    cur_author = getAuthor(cur_user_name)
 
 
     dynamic_contain = {
@@ -94,21 +95,50 @@ def make_post(request):
     # post = {}
 
     # hard coding ??
-    cur_author = getAuthor('Ritsu Onodera')
+    cur_author = getAuthor(cur_user_name)
     # ??
-
+    # cur_user_name = cur_author.DISPLAY_NAME # or we can use global
     # testcase
     dynamic_contain = {  
         'fullName':'Ritsu Onodera',
         
-        'test_name': cur_author.DISPLAY_NAME
+        'test_name': cur_user_name
     }  
     # Get the current pages' author
 
+    if request.method == "GET":
+
+        return render(request, "chat/feed.html", dynamic_contain)
+
+    elif request.method == "POST":
+
+        request_post = request.POST
+        info = request.POST.get("post_content", "")
+        # print("---------/n",request_post)
+
+        title = "Noli flere"
+        source = cur_user_name # Who share it to me
+        origin = cur_user_name # who origin create 
+        description = "Noli flere"
+        content_type = "Latin"
+        content = info
+        author = cur_author
+        categories = "text"
+        visibility = ""
+
+        createFlag = createPost(title, source, origin, description, content_type, content, author, categories, visibility)
+        if createFlag:
+            print("haha, successful create post, info: ", info)
+
+        else:
+            print("sever feels sad ", info)
+
+        return render(request, "chat/feed.html", dynamic_contain)
 
 
-    # get post
-    return render(request, "chat/feed.html", dynamic_contain)
+
+    # # get post
+    # return render(request, "chat/feed.html", dynamic_contain)
 
 def profile(request):
     author = {}
