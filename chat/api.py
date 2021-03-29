35e8de440a637fef540e7893986f87bc8b0b0100
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-03-29 15:11:10 -0600

    use get method when retrieving value of the request header x-server

--- chat/api.py
+++ chat/api.py
@@ -103,7 +103,7 @@ def post_obj(request, AUTHOR_ID, POST_ID):
 
     print("post id: ", POST_ID)
     print(request.META)
-    server_origin = request.META["HTTP_X_SERVER"]
+    server_origin = request.META.get("HTTP_X_SERVER")
     print(server_origin)
 
     if server_origin != host_server :
@@ -166,7 +166,7 @@ def posts_obj(request, AUTHOR_ID):
     # ex. equest.META[origin] == ("https:\\chatbyte"):
     # req_origin = request.META["Origin"]
     USER_ID = (AUTHOR_ID + '.')[:-1]
-    server_origin = request.META["HTTP_X_SERVER"]
+    server_origin = request.META.get("HTTP_X_SERVER")
     AUTHOR_ID = host_server + "author/" + AUTHOR_ID
     print("author id: ", AUTHOR_ID)
 
@@ -238,7 +238,7 @@ def comment_list_obj(request, AUTHOR_ID, POST_ID):
     # ex. equest.META[origin] == ("https:\\chatbyte"):
     # req_origin = request.META["Origin"]
     USER_ID = (AUTHOR_ID + '.')[:-1]
-    server_origin = request.META["HTTP_X_SERVER"]
+    server_origin = request.META.get("HTTP_X_SERVER")
     AUTHOR_ID = host_server + "author/" + AUTHOR_ID
     print("author id: ", AUTHOR_ID)
     USER_POST_ID = POST_ID
@@ -324,7 +324,7 @@ def profile_obj(request, AUTHOR_ID):
     # ex. equest.META[origin] == ("https:\\chatbyte"):
     # req_origin = request.META["Origin"]
     USER_ID = (AUTHOR_ID + '.')[:-1]
-    server_origin = request.META["HTTP_X_SERVER"]
+    server_origin = request.META.get("HTTP_X_SERVER")
     print("Origin: ", host_server)
     AUTHOR_ID = host_server + "author/" + AUTHOR_ID
     print("author id: ", AUTHOR_ID)
@@ -374,7 +374,7 @@ def follower_obj(request, AUTHOR_ID, FOREIGN_AUTHOR_ID):
     # ex. request.META[origin] == ("https:\\chatbyte"):
     # req_origin = request.META["Origin"]
     USER_ID = (AUTHOR_ID + '.')[:-1]
-    server_origin = request.META["HTTP_X_SERVER"]
+    server_origin = request.META.get("HTTP_X_SERVER")
     AUTHOR_ID = host_server + "author/" + AUTHOR_ID
     print("author id: ", AUTHOR_ID)
     FOREIGN_USER_ID = FOREIGN_AUTHOR_ID
@@ -449,7 +449,7 @@ def follower_obj(request, AUTHOR_ID, FOREIGN_AUTHOR_ID):
 def followers_obj(request, AUTHOR_ID):
     # ex. equest.META[origin] == ("https:\\chatbyte"):
     # req_origin = request.META["Origin"]
-    server_origin = request.META["HTTP_X_SERVER"]
+    server_origin = request.META.get("HTTP_X_SERVER")
     AUTHOR_ID = host_server + "author/" + AUTHOR_ID
     print("author id: ", AUTHOR_ID)
 
@@ -488,7 +488,7 @@ def followers_obj(request, AUTHOR_ID):
 @api_view(['GET'])
 def get_friends_obj(request, AUTHOR_ID):
     # req_origin = request.META["Origin"]
-    server_origin = request.META["HTTP_X_SERVER"]
+    server_origin = request.META.get("HTTP_X_SERVER")
     AUTHOR_ID = host_server + "author/" + AUTHOR_ID
     print("author id: ", AUTHOR_ID)
 
@@ -544,7 +544,7 @@ Response Object Structure: [list of Like objects]
 @api_view(['GET'])
 def likes_post_obj(request, AUTHOR_ID, POST_ID):
     # req_origin = request.META["Origin"]
-    server_origin = request.META["HTTP_X_SERVER"]
+    server_origin = request.META.get("HTTP_X_SERVER")
     AUTHOR_ID = host_server + "author/" + AUTHOR_ID
     print("author id: ", AUTHOR_ID)
     POST_ID = AUTHOR_ID + "/posts/" + POST_ID
@@ -570,7 +570,7 @@ def likes_post_obj(request, AUTHOR_ID, POST_ID):
 @api_view(['GET'])
 def likes_comment_obj(request, AUTHOR_ID, POST_ID, COMMENT_ID):
     # req_origin = request.META["Origin"]
-    server_origin = request.META["HTTP_X_SERVER"]
+    server_origin = request.META.get("HTTP_X_SERVER")
     AUTHOR_ID = host_server + "author/" + AUTHOR_ID
     print("author id: ", AUTHOR_ID)
     POST_ID = AUTHOR_ID + "/posts/" + POST_ID
@@ -601,7 +601,7 @@ def likes_comment_obj(request, AUTHOR_ID, POST_ID, COMMENT_ID):
 def liked_post_obj(request, AUTHOR_ID):
 
     # req_origin = request.META["Origin"]
-    server_origin = request.META["HTTP_X_SERVER"]
+    server_origin = request.META.get("HTTP_X_SERVER")
     AUTHOR_ID = host_server + "author/" + AUTHOR_ID
     print("author id: ", AUTHOR_ID)
 
@@ -635,7 +635,7 @@ def inbox(request, AUTHOR_ID):
     #
     else:
         # req_origin = request.META["Origin"]
-        server_origin = request.META["HTTP_X_SERVER"]
+        server_origin = request.META.get("HTTP_X_SERVER")
     # Profile/AUTHOR ID is the full url
     AUTHOR_ID = host_server + "author/" + AUTHOR_ID
     print("author id: ", AUTHOR_ID)
