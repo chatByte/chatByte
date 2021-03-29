37f0bfcd4e22fa70711afb7d86fea2a32c3e1e0f
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-03-28 18:11:23 -0600

    add liked attribute when create user profile

--- chat/signals.py
+++ chat/signals.py
@@ -3,7 +3,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
-from .models import Comment, Post, Profile, Inbox, PostInbox
+from .models import Comment, Post, Profile, Inbox, PostInbox, Liked
 
 host = "http://localhost:8000/"
 
@@ -22,12 +22,13 @@ def update_profile_signal(sender, instance, created, **kwargs):
             inbox.post_inbox = PostInbox.objects.create()
         Token.objects.create(user=instance)
         
-    instance.profile.displayName = instance.username
-    instance.profile.id = host + "author/" + str(instance.id)
-    instance.profile.save()
-    instance.inbox.post_inbox.author = instance.id
-    instance.inbox.post_inbox.save()
-    instance.inbox.save()
+        instance.profile.displayName = instance.username
+        instance.profile.id = host + "author/" + str(instance.id)
+        instance.profile.liked = Liked.objects.create()
+        instance.profile.save()
+        instance.inbox.post_inbox.author = instance.id
+        instance.inbox.post_inbox.save()
+        instance.inbox.save()
 
 @receiver(post_save, sender=Post)
 def create_post_signal(sender, instance, created, **kwargs):
