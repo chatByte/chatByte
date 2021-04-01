593f1313f632c32174589d26c99875a77d899695
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-04-01 04:06:36 -0600

    fixed print request body error

--- chat/api.py
+++ chat/api.py
@@ -737,7 +737,7 @@ def inbox(request, AUTHOR_ID):
     else:
         print("Request: ", request)
         # print("Request data: ", request.data)
-        print("Request body: ", request.body)
+        # print("Request body: ", request.body)
         if request.method == "POST":
             user = User.objects.get(pk=USER_ID)
             # print(request.data)
