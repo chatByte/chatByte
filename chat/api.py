e7c1d68d3ef0ef413ec98666fb81a7eeb011812c
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-03-31 22:14:39 -0600

    fixed header problem

--- chat/api.py
+++ chat/api.py
@@ -851,7 +851,7 @@ def stream_obj(request, AUTHOR_ID):
 
     # req_origin = request.META["Origin"]
     server_origin = request.META.get("HTTP_X_SERVER")
-    origin_server = request.META.get("Origin")
+    origin_server = request.META.get("HTTP_ORIGIN")
     if origin_server is not None and origin_server != host_server:
         AUTHOR_ID = origin_server + "author/" + AUTHOR_ID
     else:
@@ -859,7 +859,6 @@ def stream_obj(request, AUTHOR_ID):
     print("author id: ", AUTHOR_ID)
     print("Origin header: ", origin_server)
     print(request.META)
-    print(request.headers)
 
     if server_origin is not None and server_origin != host_server:
         print("Remote request body: ", request.data)
