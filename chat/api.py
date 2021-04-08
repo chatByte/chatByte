490c1413fd2b0d3a8b09aaf5715618881e531230
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-04-07 23:06:35 -0600

    fixing error in api likes post obj

--- chat/api.py
+++ chat/api.py
@@ -617,11 +617,11 @@ def likes_post_obj(request, AUTHOR_ID, POST_ID):
         except Post.DoesNotExist:
             return JsonResponse({'status':'false','message':'post id: ' + POST_ID + ' does not exists'}, status=404)
         likes = post.likes
-        serializer = LikeSerializer(data=likes, many=True)
-        if serializer.is_valid(raise_exception=True):
-            return JsonResponse(serializer.data, status=200)
+        serializer = LikeSerializer(likes, many=True)
+        # if serializer.is_valid(raise_exception=True):
+        return JsonResponse(serializer.data, status=200)
 
-        return JsonResponse(serializer.errors, status=400)
+        # return JsonResponse(serializer.errors, status=400)
 
 @csrf_exempt
 @authentication_classes([CsrfExemptSessionAuthentication, BasicAuthentication])
