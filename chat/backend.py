fcc51f91140227a4e0154d9bc2f2cd257368587c
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-04-10 11:44:25 -0600

    return commment object when create comment

--- chat/backend.py
+++ chat/backend.py
@@ -289,7 +289,7 @@ def createComment(author, post_id, comment, content_type, published=django.utils
         print("post_count:", post.count)
         post.count += 1
         post.save()
-        return True
+        return commentObj
     except BaseException as e:
         print(repr(e))
         traceback.print_exc()
