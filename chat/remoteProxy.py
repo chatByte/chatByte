b1ac54d29167978e2d1f48473a1c93913cb3ec8d
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-04-10 12:50:59 -0600

    fixed endling slash in remote inbox request

--- chat/remoteProxy.py
+++ chat/remoteProxy.py
@@ -103,7 +103,7 @@ def inboxRequest(method, origin, user_id, data=None):
     The body of the request is empty if it is a GET/DELETE request, otherwise, the body is
     the inbox in json format.
     '''
-    url = str(origin) + "author/" + str(user_id) + "/inbox/"
+    url = str(origin) + "author/" + str(user_id) + "/inbox"
     print("remote URL: ", url)
     print("remote origin: ", origin)
     user = User.objects.get(last_name=origin)
