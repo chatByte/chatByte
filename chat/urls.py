ca5c6961bc4d05eddac4adc237954765832607e0
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-04-10 13:08:35 -0600

    removed ending slash in url post request

--- chat/urls.py
+++ chat/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path(r"author/<str:AUTHOR_ID>/my_posts/<str:POST_ID>/edit/", views.update_post, name="update_posts"),
     
     path(r'author/<str:AUTHOR_ID>/posts/',api.posts_obj, name='make_posts_obj'),
-    path(r'author/<str:AUTHOR_ID>/posts/<str:POST_ID>/',api.post_obj, name='make_post_obj'),
+    path(r'author/<str:AUTHOR_ID>/posts/<str:POST_ID>',api.post_obj, name='make_post_obj'),
     path(r'author/<str:AUTHOR_ID>/posts/<str:POST_ID>/comments',api.comment_list_obj, name='comment_list_obj'),
 
 
