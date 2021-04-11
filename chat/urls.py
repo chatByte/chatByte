0ac90679c1f6f9921d6d592bd41c2a1e2f943600
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-04-11 00:17:37 -0600

    fixing get likes api for post and comment

--- chat/urls.py
+++ chat/urls.py
@@ -88,7 +88,7 @@ urlpatterns = [
     path("author/<str:AUTHOR_ID>/friends/", api.get_friends_obj, name = "get_friends"),
 
     #Get likes for a Post
-    path("author/<str:AUTHOR_ID>/posts/<str:POST_ID>/likes/", api.likes_post_obj, name="likes_post"),
+    path("author/<str:AUTHOR_ID>/posts/<str:POST_ID>/likes", api.likes_post_obj, name="likes_post"),
     path("author/<str:AUTHOR_ID>/inbox", api.inbox, name="inbox"),
 
     # # Get likes for a Comment
