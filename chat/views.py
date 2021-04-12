3569703b72744db3b1be659accf9569ea2a36a75
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-04-12 15:39:07 -0600

    combining server view function and html

--- chat/views.py
+++ chat/views.py
@@ -970,9 +970,9 @@ def unlisted(request, AUTHOR_ID, POST_ID):
     try:
         post = Post.objects.get(id=post_id)
         if post.unlisted:
-            return render(request, "chat/unlisted.html", {"unlisted_post": post})
+            return render(request, "chat/posts_unlisted.html", {"unlisted_post": post})
         else:
-            return render(request, "chat/unlisted.html", {})
+            return render(request, "chat/posts_unlisted.html", {})
     except:
-        return render(request, "chat/unlisted.html", {})
+        return render(request, "chat/posts_unlisted.html", {})
     
